2e02e5ea1b9b4a8fbdf0f9a708ad6e71ec0e7084
sl4mmer <sl4mmer@list.ru>
2018-11-16 02:15:42 +0300

    тест todo приложения

--- conf.py
+++ conf.py
@@ -3,6 +3,15 @@ import logging
 from eth_rpc_api import ParityEthJsonRpc
 
 
+# @todo Нужно покрыть тестом аггрегироваанную информацию о транзакции
+def test_config(binder):
+    logging.basicConfig(level=logging.INFO)
+    binder.bind_to_constructor(ParityEthJsonRpc,
+                               lambda: ParityEthJsonRpc("mainnet.infura.io/v3/1f55c0f454d8475784d7b73d3d6d3f6e",
+                                                        tls=True))
+
+
+
 def base_config(binder):
     logging.basicConfig(level=logging.INFO)
     binder.bind_to_constructor(ParityEthJsonRpc, lambda: ParityEthJsonRpc("status.moonshrd.io"))
