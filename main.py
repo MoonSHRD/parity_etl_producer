ae8f86ecd874ce5748b31437d6c4006da934879b
Nikita <n.metelkin@madcrush.ru>
2018-11-27 15:47:06 +0300

    подключено к центрифуге, транзы отправляются в каналы

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import argparse
+import getopt
+import json
 import logging
 import traceback
+import sys
 
 import inject
-from py._log.log import Producer
 
 from block_inspector import BlockInspector
 from conf import base_config
@@ -10,6 +13,7 @@ from extractors.EtherTransferExtractor import EtherTransferExtractor
 from extractors.TokenTransferExtractor import TokenTransferExtractor
 from mappers.transaction_mapper import TransactionMapper
 from models.transaction import Transaction
+from cent import Client
 
 logger = logging.getLogger()
 
@@ -57,17 +61,38 @@ def get_tx_value_transfers(transaction: Transaction):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-c', default="./config.json", help='path to centrifugo config')
+    parser.add_argument('-p', default="8000", help='centrifugo port')
+
+    args = parser.parse_args()
+    print(args)
+
+
+    # centrifugo
+    url = "http://localhost:"+args.__dict__['p']
+    json_data = open(args.__dict__['c']).read()
+    data = json.loads(json_data)
+    api_key = data['api_key']
+    client = Client(url, api_key=api_key, timeout=5)
+
     inject.configure(base_config)
 
     inspector = BlockInspector()
-    producer = inject.attr(Producer)
 
     while True:
         block = inspector.get_next_block()
 
-        txs = import_block()
+        txs = import_block(block)
 
-        for data in txs:
-            producer.poll(0)
-            producer.produce('transactions', TransactionMapper.transaction_to_dict(data), callback=delivery_report)
-            producer.flush()
+        for txsData in txs:
+            print(txs)
+            for key in txsData.totals:
+                u_channel="totals#"+key
+                c_clients = client.presence(u_channel)
+                print(txsData.totals)
+                if len(c_clients)>0:
+                    for key1 in txsData.totals[key]:
+                        data=txsData.totals[key][key1]
+                        print(u_channel)
+                        client.publish(u_channel, data)
